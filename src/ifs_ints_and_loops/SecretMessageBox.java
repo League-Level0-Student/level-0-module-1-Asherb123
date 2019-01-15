c923e366a25d3c5253cd4592730c5d10b2cd9813
League <league@WTS8.local>
2019-01-14 18:28:01 -0800

    Day one level 0

--- src/ifs_ints_and_loops/SecretMessageBox.java
+++ src/ifs_ints_and_loops/SecretMessageBox.java
@@ -17,16 +17,27 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
+	public static void main(String[] args) {
+		
 	
 	// 1. Set a password in a String variable
-	
+	int Password=123;
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-
+	JOptionPane.showMessageDialog(null, "Do you want to hear a secret?");
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
 	//    if they can guess the passcode
-
+	String A=JOptionPane.showInputDialog("If you do... Enter the password.");
 	// 4. If their guess matches the password, show them the secret message
-
+	if (A.equals(Password)) {
+		JOptionPane.showMessageDialog(null, "Correct!!!! But..... There was no secret");
+	}
+		
+	
+		
+	
 	// 5. If the password does not match, pop-up "INTRUDER!!"
-
+	if (A) {
+		
+	}
+}
 }
