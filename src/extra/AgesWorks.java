4e1907d995c6f49fb3cbb67fc0fed6e3240cee8d
League <league@WTS8.attlocal.net>
2019-01-28 18:30:09 -0800

    Level 0 day 3 Badger and Ages(IP)

---
+++ src/extra/AgesWorks.java
@@ -0,0 +1,26 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class AgesWorks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int S = 1;
+
+		int A = 2007;
+
+		for (int i = 1; i < 12; i++) {
+			JOptionPane.showMessageDialog(null, " In"+ (2007+i)+ "I turned 1 years old."+i);
+
+			
+			JOptionPane.showMessageDialog(null, "Press next.");
+			System.out.println("I was" +i+ "years old.");
+			System.out.println("in"+ ("2007" +i));   
+
+		}
+
+	}
+
+}
---
+++ src/extra/BadgerSong.java
@@ -0,0 +1,38 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class BadgerSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 0; i < 3; i++) {
+			JOptionPane.showMessageDialog(null, "Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger\n" + 
+					"Mushroom, mushroom, a");
+		}
+		
+		
+		for (int i = 0; i < 1; i++) {
+			JOptionPane.showMessageDialog(null, "Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger\n" + 
+					"A snake, a snake\n" + 
+					"Snake! A snake, ooh it's a snake");
+		}
+		
+		for (int i = 0; i < 1; i++) {
+			JOptionPane.showMessageDialog(null, "It's a badger, badger, badger, badger, badger, badger, badger, badger, badger, badger\n" + 
+					"Mushroom, mushroom, a\n" + 
+					"");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	
+
+
+}}
